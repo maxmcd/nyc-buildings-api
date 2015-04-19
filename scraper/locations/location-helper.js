8b56717d701f1c6de979f09efb6246856628edf9
maxmcd <max.t.mcdonnell@gmail.com>
2015-04-18 23:39:30 -0400

    locations readme, better formatted json  - metadate removed from json  - better attribute names  - go struct conversion example in readme  - updated bookmarklet helper with new formatting

--- scraper/locations/location-helper.js
+++ scraper/locations/location-helper.js
@@ -26,7 +26,7 @@ var cssPath = function(el) {
 };
 var jsonCreator = function(e) {
 	var path = cssPath(e.toElement);
-	console.log("\"\": {\n\t\"location\" : \""+
+	console.log("{\n\t\"name\":\"\",\n\t\"location\" : \""+
 				path + "\",\n\t\"regex\" : \""+
 				e.toElement.innerText + "\"\n},");
 };
@@ -34,4 +34,4 @@ document.onclick = jsonCreator;
 
 
 // as bookmarklet
-javascript:(function(){var cssPath = function(el) { if (!(el instanceof Element)) return; var path = []; while (el.nodeType === Node.ELEMENT_NODE) { var selector = el.nodeName.toLowerCase(); if (el.id) { selector += '#' + el.id; path.unshift(selector); break; } else { var sib = el, nth = 1; while (sib = sib.previousElementSibling) { if (sib.nodeName.toLowerCase() == selector) nth++; } if (nth != 1) selector += ":nth-of-type(" + nth + ")"; } path.unshift(selector); el = el.parentNode; } return path.join(" > "); }; var jsonCreator = function(e) { var path = cssPath(e.toElement); console.log("\"\": {\n\t\"location\" : \""+ path + "\",\n\t\"regex\" : \""+ e.toElement.innerText + "\"\n},"); }; document.onclick = jsonCreator;})();
+javascript:(function(){var cssPath = function(el) { if (!(el instanceof Element)) return; var path = []; while (el.nodeType === Node.ELEMENT_NODE) { var selector = el.nodeName.toLowerCase(); if (el.id) { selector += '#' + el.id; path.unshift(selector); break; } else { var sib = el, nth = 1; while (sib = sib.previousElementSibling) { if (sib.nodeName.toLowerCase() == selector) nth++; } if (nth != 1) selector += ":nth-of-type(" + nth + ")"; } path.unshift(selector); el = el.parentNode; } return path.join(" > "); }; var jsonCreator = function(e) { var path = cssPath(e.toElement); console.log("{\n\t\"name\":\"\",\n\t\"location\" : \""+ path + "\",\n\t\"regex\" : \""+ e.toElement.innerText + "\"\n},"); }; document.onclick = jsonCreator;})();
