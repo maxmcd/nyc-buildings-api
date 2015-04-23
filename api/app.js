a5eaaf85f359359782d4fa030871ed7fe50995fa
Kuan Butts <kuanbutts@yahoo.com>
2015-04-22 21:03:24 -0400

    restructure json response

--- api/app.js
+++ api/app.js
@@ -1,6 +1,9 @@
 var restify   = require('restify');
 var server    = restify.createServer({
 	name: 'nyc-buildings-api',
+	formatters: {
+		'application/json': jsonFormatter
+	},
 });
 
 var fs        = require("fs");
@@ -43,13 +46,51 @@ var Building  = sequelize.define('buildings', {
 // -----------------------------
 // Endpoints
 // -----------------------------
-server.get('/building/:bbl', function(req, res) {
-    var bbl = req.params.lot;
+
+server.get('/building/:bin', function(req, res) {
+    var bin = req.params.bin;
 
     Building
-    	.findOne({where:{bbl:bbl}})
-  		.complete(function (err, data) {
- 				res.send(data);
+    	.findOne({where:{bin:bin}})
+    	.complete(function (err, data) {
+ 				if (data != null) {
+ 					var data = data.dataValues;
+ 					var output = {
+						bbl                        : data.bbl,
+						bin                        : data.bin,
+						health_area                : data.health_area,
+						census_tract               : data.census_tract,
+						community_board            : data.community_board,
+						buildings_on_lot           : data.buildings_on_lot,
+						tax_block                  : data.tax_block,
+						condo                      : data.condo,
+						vacant                     : data.vacant,
+						cross_streets              : data.cross_streets,
+						dob_special_place_name     : data.dob_special_place_name
+						landmark_status            : data.landmark_status
+						local_law                  : data.local_law,
+						environmental_restrictions : data.environmental_restrictions,
+						legal_adult_use            : data.legal_adult_use,
+						loft_law                   : data.loft_law,
+						special_status             : data.special_status,
+						city_owned                 : data.city_owned,
+						special_district           : data.special_district,
+						complaints                 : {
+							total                    : data.complaints_total,
+							open                     : data.complaints_open,
+						},
+						violations                 : {
+							dob_total                : data.violations_dob_total,
+							dob_open                 : data.violations_dob_open,
+							ecb_total                : data.violations_ecb_total,
+							ecb_open                 : data.violations_ecb_open,
+						},
+ 					}
+ 					res.send(output);
+ 				}
+ 				else {
+ 					res.send("FUCK THE WORLD")
+ 				}
  			});
   }
 );
@@ -90,7 +131,41 @@ server.get('/another', function(req, res) {
 	});	
 })
 
-
+// -----------------------------
+// Utilities
+// -----------------------------
+function jsonFormatter(req, res, body) {
+  if (!body) {
+	if (res.getHeader('Content-Length') === undefined &&
+		res.contentLength === undefined) {
+	  res.setHeader('Content-Length', 0);
+	}
+	return null;
+  }
+ 
+  if (body instanceof Error) {
+	// snoop for RestError or HttpError, but don't rely on instanceof
+	if ((body.restCode || body.httpCode) && body.body) {
+	  body = body.body;
+	} else {
+	  body = {
+		message: body.message
+	  };
+	}
+  }
+ 
+  if (Buffer.isBuffer(body))
+	body = body.toString('base64');
+ 
+  var data = JSON.stringify(body, null, 2);
+ 
+  if (res.getHeader('Content-Length') === undefined &&
+	  res.contentLength === undefined) {
+	res.setHeader('Content-Length', Buffer.byteLength(data));
+  }
+ 
+  return data;
+}
 
 
 // -----------------------------
